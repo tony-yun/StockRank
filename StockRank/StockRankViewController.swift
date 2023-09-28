fd57dbc5bc9a29fdee4091947278e0d6b8012cd3
tony-yun <lifeyun24@naver.com>
2023-09-28 15:02:07 +0900

    set autolayout for one collection view cell

---
+++ StockRank/StockRankViewController.swift
@@ -0,0 +1,20 @@
+//
+//  StockRankViewController.swift
+//  StockRank
+//
+//  Created by 윤태웅 on 9/28/23.
+//
+
+import UIKit
+
+class StockRankViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
